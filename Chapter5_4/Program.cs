d1ce2a8d98652c7e6290d8008a431e978fd0476d
Шемаев Кирилл <shemaev.kirill@yandex.ru>
2022-10-20 21:57:11 +0300

    chapter 6 start

---
+++ Chapter5_4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5_4
+{
+	class Person
+	{
+		private byte _age;
+
+		public const double PI = 3.14;
+		public readonly double E = 2.72;
+
+		public string Name { get; set; }
+		public byte Age
+		{
+			get => _age;
+			set 
+			{
+				checked
+				{
+					_age = value > 0 ? value : 0;
+				}
+			}
+		}
+		public Person(string _Name = "", byte _Age = 0)
+		{
+			Name = _Name;
+			Age = _Age;
+		}
+	}
+	class Benefit
+	{
+		public int GetBenefitCost()
+		{
+			return 125;
+		}
+	}
+	class Employee: Person
+	{
+		public Benefit Benefit { get; set; } = new Benefit();// делегация
+		public int GetBenefit()				// добавление функционала для содержащегося объекта
+		{                                   // делегация
+			return Benefit.GetBenefitCost();
+		}
+
+		private int? _personalNumber;
+		public int? PersonalNumber
+		{
+			get => _personalNumber;
+			set
+			{
+				if ((value / 10000) < 10 && (value / 10000) != 0)
+				{
+					_personalNumber = value;
+				}
+				else
+				{
+					_personalNumber = null;
+				}
+			}
+		}
+		public Employee(string _Name = "", byte _Age = 0, int? numb = null) : base(_Name, _Age)
+		{
+			PersonalNumber = numb;
+		}
+		public override string ToString()
+		{
+			return $"{Name} {Age} years old; Number: {PersonalNumber}\n";
+		}
+
+	}
+	
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person p = new Person() { Name = "35", Age = 234 };
+			Person.PI.ToString(); // константное поле является неявно статическим
+			Console.WriteLine(p.E.ToString()); // поля readonly не являются статическими. Можно задать в конструкторе
+
+			Employee employee = new Employee("Kirya", 19, 14124);
+			Employee employee1 = new Employee("Danya", 18);
+			Employee employee2 = new Employee("Vlad", 19, 141243);
+			Console.WriteLine($"{employee}{employee1}{employee2}");
+			Console.WriteLine(employee2.GetBenefit());
+		}
+	}
+}
